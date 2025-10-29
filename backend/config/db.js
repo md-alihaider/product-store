8905e5202dd35d69bef29011ac8c10c1d012d9cd
Md Ali Haider <alihaiderbgp85@gmail.com>
2025-10-29 14:37:11 +0530

    project completed

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1); // code 1 means that there is error, 0 means success
+  }
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Box, useColorModeValue } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import Navbar from './components/Navbar.jsx'
+
+ 
+const App = () => {
+  return (
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App
